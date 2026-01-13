b72b0506f2cf012e94c160e9e118108245f4daf8
Jennie Modd <jenniemodd@hotmail.com>
2026-01-13 19:15:09 +0100

    skapade produkter med kvalifikationer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
 PSEUDOKOD – WEBBSHOP MED MUNKAR
 
-1. Skapa en array med produkter
+1. Skapa en array med produkterna för munkarna: plain, chocolate och sugar
    Varje produkt ska ha:
    - name
    - price
@@ -33,3 +33,28 @@ PSEUDOKOD – WEBBSHOP MED MUNKAR
 7. Vid klick på "visa alla":
    - skicka hela products-arrayen till renderProducts
 */
+
+
+const plainMunk = {
+  name: "Plain Munk",
+  price: 40,
+  rating: 4,
+  category: "plain",
+  image: "images/plain-munk.jpg"
+};
+
+const chocolateMunk = {
+  name: "Chocolate Munk",
+  price: 45,
+  rating: 4.5,
+  category: "chocolate",
+  image: "images/chocolate-munk.jpg"
+};
+
+const sugarMunk = {
+  name: "Sugar Munk",
+  price: 35,
+  rating: 4.2,
+  category: "sugar",
+  image: "images/sugar-munk.jpg"
+};
